82a3d072e8f736a393e4662aac9ae09b1d3576d9
Sree Harsha <95706697+Sree-Harsha-PS@users.noreply.github.com>
2022-10-04 13:43:23 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+/*var React = require("react");
+var ReactDOM = require("react-dom");
+
+ReactDOM.render(<h1>Namaste</h1>,document.getElementById("root"))
+
+var h1 = document.createElement("h1");
+h1.innerHTML="Namaste";
+document.getElementById("root").appendChild(h1);*/
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+<div>
+  <h1>Hello World</h1>
+  <p>Jai Sri Ganesh</p>
+</div>, document.getElementById("root"));
+
